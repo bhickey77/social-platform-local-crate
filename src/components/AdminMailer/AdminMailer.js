3fbcb746fc8573356a24b4b23398dd380c7ca6fc
JacobTovsen <jacobtovsen@gmail.com>
2018-07-27 14:35:33 -0500

    building out mailer

--- src/components/AdminMailer/AdminMailer.js
+++ src/components/AdminMailer/AdminMailer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
@@ -19,15 +20,56 @@ class AdminMailer extends Component {
     }
   }
 
+  handleSubmit(e){
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    axios({
+        method: "POST", 
+        url:"http://localhost:3002/send", 
+        data: {
+            name: name,   
+            email: email,  
+            messsage: message
+        }
+    }).then((response)=>{
+        if (response.data.msg === 'success'){
+            alert("Message Sent."); 
+            this.resetForm()
+        }else if(response.data.msg === 'fail'){
+            alert("Message failed to send.")
+        }
+    })
+  }
+
+  resetForm(){
+    document.getElementById('contact-form').reset();
+  }
+
   render() {
     let content = null;
 
     if (this.props.user.userName) {
       content = (
         <div>
-          <p>
-            Admin Page
-          </p>
+            <form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
+            {/* <form id="contact-form" method="POST"> */}
+            <div className="form-group">
+            <label for="name">Name</label>
+            <input type="text" className="form-control" id="name" />
+        </div>
+        <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+        </div>
+        <div className="form-group">
+            <label for="message">Message</label>
+            <textarea className="form-control" rows="5" id="message"></textarea>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+         
         </div>
       );
     }
--- src/components/PartnerMailUrl/PartnerMailUlr.js
+++ src/components/PartnerMailUrl/PartnerMailUlr.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
 import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
 
@@ -9,20 +8,16 @@ const mapStateToProps = state => ({
 });
 
 class PartnerMailUrl extends Component {
-  componentDidMount() {
-    this.props.dispatch({type: USER_ACTIONS.FETCH_USER});
-  }
+ 
 
   componentDidUpdate() {
-    if (!this.props.user.isLoading && this.props.user.userName === null) {
-      this.props.history.push('home');
-    }
+    
   }
 
   render() {
     let content = null;
 
-    if (this.props.user.userName) {
+    
       content = (
         <div>
           <p>
@@ -30,7 +25,7 @@ class PartnerMailUrl extends Component {
           </p>
         </div>
       );
-    }
+    
 
     return (
       <div>
