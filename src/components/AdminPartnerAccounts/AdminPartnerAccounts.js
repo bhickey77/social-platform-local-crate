8ae244bfc91f0292e137472fd813af9d16c2f3e6
Bill Hickey <bhickey77@gmail.com>
2018-08-10 15:57:19 -0500

    partner table styling

--- src/components/AdminPartnerAccounts/AdminPartnerAccounts.js
+++ src/components/AdminPartnerAccounts/AdminPartnerAccounts.js
@@ -39,17 +39,20 @@ class AdminPartnerAccounts extends Component {
         <div className='adminTable'>
         <Table>
           <TableHead>
-          <TableRow>
-            <TableCell>First Name</TableCell>
-            <TableCell>Last Name</TableCell>
-            <TableCell>City</TableCell>
-            <TableCell>State</TableCell>
-            <TableCell>Website</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Phone Number</TableCell>
-            <TableCell>Date Created</TableCell>
-            <TableCell>Date Updated</TableCell>
-            <TableCell>Type</TableCell>
+          <TableRow style={{ 
+              backgroundColor: '#00bce4', 
+              borderRadius: '5px',
+              }}>
+            <TableCell style={styles.tableHeaderCells}>First Name</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Last Name</TableCell>
+            <TableCell style={styles.tableHeaderCells}>City</TableCell>
+            <TableCell style={styles.tableHeaderCells}>State</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Website</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Email</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Phone Number</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Date Created</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Date Updated</TableCell>
+            <TableCell style={styles.tableHeaderCells}>Type</TableCell>
           </TableRow>
           </TableHead>
             <TableBody>
